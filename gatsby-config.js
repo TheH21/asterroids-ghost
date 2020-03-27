179e93318a130313d56b3ed0e1cc6510269774a4
Davido <htwenty1@gmail.com>
2020-03-26 23:26:16 -0700

    Update gatsby-config.js
    Double fixed API Ghost shtuff whoops

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ try {
 } catch (e) {
     ghostConfig = {
         production: {
-            apiUrl: process.env.http://45.76.246.63:3001,
-            contentApiKey: process.env.55ea366afca43f6177b1c7459b,
+            apiUrl: process.env.`http://45.76.246.63:3001`,
+            contentApiKey: process.env.`55ea366afca43f6177b1c7459b`,
         },
     }
 } finally {
