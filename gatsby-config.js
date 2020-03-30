3f61c6e0523b5e34f59f8c2b63b832d3f00c16ad
Davido <htwenty1@gmail.com>
2020-03-29 20:19:46 -0700

    Added backbone for ghost images

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,7 @@ module.exports = {
                 name: `images`,
             },
         },
+        `gatsby-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
         {
@@ -61,6 +62,36 @@ module.exports = {
                 process.env.NODE_ENV === `development`
                     ? ghostConfig.development
                     : ghostConfig.production,
+        },
+        {
+            resolve: `gatsby-plugin-ghost-images`,
+            options: {
+                // An array of node types and image fields per node
+                // Image fields must contain a valid absolute path to the image to be downloaded
+                lookup: [
+                    {
+                        type: `GhostPost`,
+                        imgTags: [`feature_image`],
+                    },
+                    {
+                        type: `GhostPage`,
+                        imgTags: [`feature_image`],
+                    },
+                    {
+                        type: `GhostSettings`,
+                        imgTags: [`cover_image`],
+                    },
+                ],
+                // Additional condition to exclude nodes 
+                // Takes precedence over lookup
+                exclude: node => (
+                    node.ghostId === undefined
+                ),
+                // Additional information messages useful for debugging
+                verbose: true,
+                // Option to disable the module (default: false)
+                disable: false,
+            },
         },
         /**
          *  Utility Plugins
